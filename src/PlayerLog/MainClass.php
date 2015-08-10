f8a8a6ec15089434d5fd0966158f1a81063d5193
0929hitoshi <kimurahitoshi0929@yahoo.co.jp>
2015-08-10 23:18:29 +0900

    Update MainClass.php

--- src/PlayerLog/MainClass.php
+++ src/PlayerLog/MainClass.php
@@ -220,7 +220,7 @@ class MainClass extends PluginBase implements Listener {
 			$player = $event->getPlayer();
 			$m = $event->getMessage();
 			if($m[0] === "/"){
-				if(strpos($m, '/register') !== false or strpos($m, '/login') !== false){
+				if(stripos($m, '/register') !== false or stripos($m, '/login') !== false){
 					$this->getLogger()->info($player->getName() . "さんがコマンド" . $this->getCommandName($m) . "を使用しました");
 				}else{
 					$this->getLogger()->info($player->getName() . "さんがコマンドを使用しました コマンド: " . $m);
